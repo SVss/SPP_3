24ec0a3e0dfc46e45108989e2000d8b214d4b3e6
SVss <savionak@gmail.com>
2016-10-14 23:11:13 +0300

    Create structure
    * add FilesListItem
    * add ThreadsListItem and MethodsListItem
    * add BadXmlException

---
+++ XmlParserWpf/FilesLisItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using TracerLib;
+
+namespace XmlParserWpf
+{
+    internal class FilesListItem
+    {
+        public string Path { get; set; }
+        public string Name => System.IO.Path.GetFileName(Path);
+        public bool IsSaved { get; set; }
+
+        public List<ThreadsListItem> ThreadsList { get; }
+
+        private FilesListItem()
+        {
+            ThreadsList = new List<ThreadsListItem>();
+        }
+
+        private static FilesListItem LoadFromFile(string path)
+        {
+            var result = new FilesListItem();
+
+            // TODO: load and parse XML-file here
+
+            return result;
+        }
+        
+        public void OnChanged()
+        {
+            IsSaved = false;
+        }
+    }
+}
---
+++ XmlParserWpf/ThreadsListItem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using TracerLib;
+
+namespace XmlParserWpf
+{
+    internal class ThreadsListItem
+    {
+        public ThreadsListItem()
+        {
+            Methods = new List<MethodsListItem>();
+        }
+
+        public long Id { get; set; }
+        public long Time { get; set; }
+        public List<MethodsListItem> Methods { get; }
+
+        public override string ToString()
+        {
+            return $"thread (id=\"{Id}\" time=\"{Time}\")";
+        }
+    }
+
+    internal class MethodsListItem
+    {
+        public MethodsListItem()
+        {
+            Children = new List<MethodsListItem>();
+        }
+
+        public string Name { get; set; }
+        public string Package { get; set; }
+        public long ParamsCount { get; set; }
+        public long Time { get; set; }
+
+        public List<MethodsListItem> Children { get; }
+
+        public override string ToString()
+        {
+            return $"{Name} (params=\"{ParamsCount}\" package=\"{Package})\" time=\"{Time}\"";
+        }
+    }
+}
