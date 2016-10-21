a26a9397400159fcc01eb9455074e3f205f1e054
SVss <savionak@gmail.com>
2016-10-21 04:32:10 +0300

    Add FileView

---
+++ XmlParserWpf/XmlParserWpf/Views/FileView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace XmlParserWpf.Views
+{
+    /// <summary>
+    /// Interaction logic for FileView.xaml
+    /// </summary>
+    public partial class FileView : UserControl
+    {
+        public FileView()
+        {
+            InitializeComponent();
+        }
+    }
+}
